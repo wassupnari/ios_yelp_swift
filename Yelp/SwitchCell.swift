3f2d3f0e20ec45e44c6c00efea348922e94ae3ea
wassupnari <wassupnari@gmail.com>
2016-10-22 16:49:55 -0700

    Added switch cell

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Nari Shin on 10/22/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
